9546736a80d7eca439490c8dae22eba63a60d754
Shubham <varshneyshubham8923011761@gmail.com>
2022-03-08 21:45:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Cropper from "react-easy-crop";
+import Slider from "@material-ui/core/Slider";
+
+
+
+
+export default function App() {
+	const inputRef = React.useRef();
+
+	const triggerFileSelectPopup = () => inputRef.current.click();
+
+	const [image, setImage] = React.useState(null);
+	const [croppedArea, setCroppedArea] = React.useState(null);
+	const [crop, setCrop] = React.useState({ x: 0, y: 0 });
+	const [zoom, setZoom] = React.useState(1);
+	const [result,setResult] = useState(null)
+
+	const onCropComplete = (croppedAreaPercentage, croppedAreaPixels) => {
+		setCroppedArea(croppedAreaPixels);
+	};
+
+	const onSelectFile = (event) => {
+		if (event.target.files && event.target.files.length > 0) {
+			const reader = new FileReader();
+			reader.readAsDataURL(event.target.files[0]);
+			reader.addEventListener("load", () => {
+				setImage(reader.result);
+			});
+		}
+	};
+
+	
+
+	return (
+		<div className='container'>
+			<div className='container-cropper'>
+				{image ? (
+					<>
+						<div className='cropper'>
+							<Cropper
+								image={image}
+								crop={crop}
+								zoom={zoom}
+								aspect={1}
+								onCropChange={setCrop}
+								onZoomChange={setZoom}
+								onCropComplete={onCropComplete}
+							/>
+						</div>
+						<div className="slider">
+							<Slider min={1} max={3} step={ 0.1} value={zoom} onChange={(e,zoom)=>setZoom(zoom)}/>
+							</div>
+
+						
+					</>
+				) : null}
+			</div>
+
+			<div className='container-buttons'>
+				<input
+					type='file'
+					accept='image/*'
+					ref={inputRef}
+					onChange={onSelectFile}
+					style={{ display: "none" }}
+				/>
+				<button
+					
+					onClick={triggerFileSelectPopup}
+					style={{ marginRight: "10px" }}
+				>
+					Choose
+				</button>
+				
+				
+			</div>
+			{result && <div> 
+				<img src="{result}" alt="Cropped image" /></div>}
+		</div>
+	);
 }
-
-export default App;
